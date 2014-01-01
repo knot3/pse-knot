e346dd07368a6b18b830babf72eb2d722a19524e
Tobias Schulz <tobias@freehal.net>
2014-01-01 20:06:03 +0100

    Implementierung einer dynamische Tastenbelegung
    neue Klassen:
    - KeyOptionInfo (erbt von DistinctOptionInfo): zum Abspeichern von Tasten in den Optionen, basierend auf dem Keys-Enum von XNA
    - EnumHelper: für einige statische Methoden zum Umgang mit Enums, die es so in .NET nicht direkt gibt
    
    neue Enums:
    - PlayerActions: alle Aktionen, für die der Spieler die zugewiesene Taste festlegen kann. Wird für eine möglichst saubere Implementierung der Tastenzuordnung benötigt.
    
    Der KnotInputHandler hat ein Dictionary<Keys, PlayerActions> für die aktuelle Tastenzuordnung und ein readonly-Dictionary für die Standard-Tastenzuordnung.
    Dazu noch ein Dictionary<PlayerActions, Action<GameTime>> für die Zuordnung zwischen dem Aktions-Enum und Lambda-Methoden, die die entsprechenden Aktionen ausführen.
    Nur so können wir vermeiden, switch zu verwenden (sonst beschwert sich ja wieder jeder, das dürfte man nicht...)
    
    Fixes #29, Betrifft #30, #13

--- Knot3/Knot3-Implementierung/Knot3/GameObjects/KnotInputHandler.cs
+++ Knot3/Knot3-Implementierung/Knot3/GameObjects/KnotInputHandler.cs
@@ -18,6 +18,8 @@ using Knot3.Screens;
 using Knot3.RenderEffects;
 using Knot3.KnotData;
 using Knot3.Widgets;
+using System.ComponentModel;
+using Knot3.Utilities;
 
 namespace Knot3.GameObjects
 {
@@ -25,7 +27,7 @@ namespace Knot3.GameObjects
 	/// Verarbeitet die Maus- und Tastatureingaben des Spielers und modifiziert die Kamera-Position
 	/// und das Kamera-Ziel.
 	/// </summary>
-	public class KnotInputHandler : GameScreenComponent
+	public class KnotInputHandler : GameScreenComponent, IKeyEventListener
 	{
         #region Properties
 
@@ -34,9 +36,45 @@ namespace Knot3.GameObjects
 		/// </summary>
 		private World world;
 
+		/// <summary>
+		/// Die Tasten, auf die die Klasse reagiert. Wird aus der aktuellen Tastenbelegung berechnet.
+		/// </summary>
 		public List<Keys> ValidKeys { get; set; }
 
+		/// <summary>
+		/// Zeigt an, ob die Klasse zur Zeit auf Tastatureingaben reagiert.
+		/// </summary>
 		public bool IsKeyEventEnabled { get { return true; } }
+
+		/// <summary>
+		/// Die aktuelle Tastenbelegung
+		/// </summary>
+		private static Dictionary<Keys, PlayerActions> CurrentKeyAssignment = new Dictionary<Keys, PlayerActions> ();
+
+		/// <summary>
+		/// Die Standard-Tastenbelegung.
+		/// </summary>
+		private static readonly Dictionary<Keys, PlayerActions> DefaultKeyAssignment
+				= new Dictionary<Keys, PlayerActions>
+		{
+			{ Keys.W, 		PlayerActions.MoveUp },
+			{ Keys.S, 		PlayerActions.MoveDown },
+			{ Keys.A, 		PlayerActions.MoveLeft },
+			{ Keys.D, 		PlayerActions.MoveRight},
+			{ Keys.R, 		PlayerActions.MoveForward },
+			{ Keys.F, 		PlayerActions.MoveBackward },
+			{ Keys.Up, 		PlayerActions.RotateUp },
+			{ Keys.Down, 	PlayerActions.RotateDown },
+			{ Keys.Left, 	PlayerActions.RotateLeft },
+			{ Keys.Right, 	PlayerActions.RotateRight },
+			{ Keys.Q, 		PlayerActions.ZoomIn },
+			{ Keys.E, 		PlayerActions.ZoomOut },
+		};
+
+		/// <summary>
+		/// Was bei den jeweiligen Aktionen ausgeführt wird.
+		/// </summary>
+		private static Dictionary<PlayerActions, Action<GameTime>> ActionBindings;
 
         #endregion
 
@@ -58,14 +96,24 @@ namespace Knot3.GameObjects
 			
 			// Tasten
 			ValidKeys = new List<Keys> ();
-			ValidKeys.AddRange (
-				new []{
-					Keys.A, Keys.D, Keys.W, Keys.S, Keys.R, Keys.F, Keys.Q, Keys.E, Keys.A, Keys.D, Keys.W,
-					Keys.S, Keys.R, Keys.F, Keys.Q, Keys.E,
-					Keys.Left, Keys.Right, Keys.Up, Keys.Down, Keys.OemPlus, Keys.OemMinus, Keys.Enter,
-					Keys.LeftAlt, Keys.Tab
-				}
-			);
+			OnControlSettingsChanged ();
+
+			// Lege die Bedeutungen der PlayerActions fest
+			ActionBindings = new Dictionary<PlayerActions, Action<GameTime>>
+			{
+				{ PlayerActions.MoveUp, 		(time) => move (Vector3.Up, time) },
+				{ PlayerActions.MoveDown, 		(time) => move (Vector3.Down, time) },
+				{ PlayerActions.MoveLeft, 		(time) => move (Vector3.Left, time) },
+				{ PlayerActions.MoveRight, 		(time) => move (Vector3.Right, time) },
+				{ PlayerActions.MoveForward, 	(time) => move (Vector3.Forward, time) },
+				{ PlayerActions.MoveBackward, 	(time) => move (Vector3.Backward, time) },
+				{ PlayerActions.RotateUp, 		(time) => rotate (-Vector2.UnitY, time) },
+				{ PlayerActions.RotateDown, 	(time) => rotate ( Vector2.UnitY, time) },
+				{ PlayerActions.RotateLeft, 	(time) => rotate (-Vector2.UnitX, time) },
+				{ PlayerActions.RotateRight, 	(time) => rotate ( Vector2.UnitX, time) },
+				{ PlayerActions.ZoomIn, 		(time) => zoom (-1, time) },
+				{ PlayerActions.ZoomOut, 		(time) => zoom (+1, time) },
+			};
 		}
 
         #endregion
@@ -106,13 +154,6 @@ namespace Knot3.GameObjects
 			// TODO
 		}
 
-		protected void UpdateKeys (GameTime time)
-		{
-			world.Redraw = true;
-
-			// TODO
-		}
-
 		private void ResetMousePosition ()
 		{
 			if (InputManager.CurrentMouseState != InputManager.PreviousMouseState) {
@@ -123,13 +164,103 @@ namespace Knot3.GameObjects
 			}
 		}
 
-		public void OnKeyEvent (List<Keys> key, KeyEvent keyEvent, GameTime time)
+		/// <summary>
+		/// Verschiebt die Kamera und das Target linear in die angegebene Richtung.
+		/// </summary>
+		private void move (Vector3 move, GameTime time)
 		{
-			UpdateKeys (time);
+			if (move.Length () > 0) {
+				move *= 10;
+				Vector3 targetDirection = world.Camera.TargetDirection;
+				Vector3 up = world.Camera.UpVector;
+				world.Camera.Target = world.Camera.Target.MoveLinear (move, up, targetDirection);
+				world.Camera.Position = world.Camera.Position.MoveLinear (move, up, targetDirection);
+				Screen.Input.CurrentInputAction = InputAction.FirstPersonCameraMove;
+			}
+		}
+
+		/// <summary>
+		/// Rotiert die Kamera auf einem Arcball um das Target.
+		/// </summary>
+		private void rotate (Vector2 move, GameTime time)
+		{
+			if (move.Length () > 0) {
+				move *= 3;
+				Vector3 targetDirection = world.Camera.TargetDirection;
+				Vector3 up = world.Camera.UpVector;
+				float oldDistance = world.Camera.TargetDistance = world.Camera.TargetDistance.Clamp (500, 10000);
+				world.Camera.Target = new Vector3 (world.Camera.ArcballTarget.X, world.Camera.Target.Y, world.Camera.ArcballTarget.Z);
+				world.Camera.Position = world.Camera.ArcballTarget
+					+ (world.Camera.Position - world.Camera.ArcballTarget).ArcBallMove (
+						move, up, targetDirection
+				);
+				world.Camera.TargetDistance = oldDistance;
+				Screen.Input.CurrentInputAction = InputAction.ArcballMove;
+			}
+		}
+
+		/// <summary>
+		/// Führt einen Zoom durch, indem die Distanz von der Kamera zum Target erhöht oder verringert wird.
+		/// </summary>
+		private void zoom (int value, GameTime time)
+		{
+			world.Camera.TargetDistance += value * 10;
+		}
+
+		public void OnKeyEvent (List<Keys> keys, KeyEvent keyEvent, GameTime time)
+		{
+			// Bei einem Tastendruck wird die Spielwelt auf jeden Fall neu gezeichnet.
+			world.Redraw = true;
+
+			// Iteriere über alle gedrückten Tasten
+			foreach (Keys key in keys) {
+				// Ist der Taste eine Aktion zugeordnet?
+				if (CurrentKeyAssignment.ContainsKey (key)) {
+					// Während die Tate gedrückt gehalten ist...
+					if (key.IsHeldDown ()) {
+						// führe die entsprechende Aktion aus!
+						PlayerActions action = CurrentKeyAssignment [key];
+						Action<GameTime> binding = ActionBindings [action];
+						binding (time);
+					}
+				}
+			}
+		}
+
+		/// <summary>
+		/// Wird ausgeführt, sobald ein KnotInputHandler erstellt wird und danach,
+		/// wenn sich die Tastenbelegung geändert hat.
+		/// </summary>
+		public void OnControlSettingsChanged ()
+		{
+			// Drehe die Zuordnung um; von (Taste -> Aktion) zu (Aktion -> Taste)
+			Dictionary<PlayerActions, Keys> defaultReversed = DefaultKeyAssignment.ToDictionary (x => x.Value, x => x.Key);
+
+			// Leere die aktuelle Zuordnung
+			CurrentKeyAssignment.Clear ();
+
+			// Fülle die aktuelle Zuordnung mit aus der Einstellungsdatei gelesenen werten.
+			// Iteriere dazu über alle gültigen PlayerActions...
+			foreach (PlayerActions action in typeof(PlayerActions).ToEnumValues<PlayerActions>()) {
+				string controlName = action.ToEnumDescription ();
+
+				// Erstelle eine Option...
+				KeyOptionInfo option = new KeyOptionInfo (
+					section: "controls",
+					name: controlName,
+					defaultValue: defaultReversed [action],
+					configFile: Options.Default
+				);
+				// und lese den Wert aus und speichere ihn in der Zuordnung.
+				CurrentKeyAssignment [option.Value] = action;
+			}
+
+			// Aktualisiere die Liste von Tasten, zu denen wir als IKeyEventListener benachrichtigt werden
+			ValidKeys.Clear ();
+			ValidKeys.AddRange (CurrentKeyAssignment.Keys.AsEnumerable ());
 		}
 
         #endregion
-
 	}
 }
 
