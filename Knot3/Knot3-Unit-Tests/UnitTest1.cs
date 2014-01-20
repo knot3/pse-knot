5c0c8f3f0de9698f3da105ae3e754eede9990ef3
Tobias Schulz <tobias@freehal.net>
2014-01-20 22:34:10 +0100

    Tests funktionieren jetzt auch unter Linux

--- Knot3/Knot3-Unit-Tests/UnitTest1.cs
+++ Knot3/Knot3-Unit-Tests/UnitTest1.cs
@@ -1,27 +1,23 @@
 ﻿using System;
 
-
-
 using NUnit.Framework;
 
-
-
-namespace Knot3_Unit_Tests
+namespace Knot3.UnitTests
 {
-    [TestFixture]
-    public class UnitTest1
-    {
-        [Test]
-        public void TestMethod1()
-        {
-            Assert.IsNull(null);
-        }
+	[TestFixture]
+	public class UnitTest1
+	{
+		[Test]
+		public void TestMethod1 ()
+		{
+			Assert.IsNull (null);
+		}
 
-        [Test]
-        public void TestMethod2()
-        {
-            Assert.IsNotNull(null);
-        }
-    }
+		[Test]
+		public void TestMethod2 ()
+		{
+			Assert.IsNotNull (null);
+		}
+	}
 }
 
