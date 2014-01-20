f23d9f5417bc0dad16c1c61b50890a6b2e153d92
Pascal Knodel <pascal.knodel@student.kit.edu>
2014-01-20 17:04:06 +0100

    NUnit Komponententests bei mir installiert und probiert.
    TODO: Jeder muss VS bei sich noch entsprechend einrichten.

---
+++ Knot3/Knot3-Unit-Tests/UnitTest1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+
+
+using NUnit.Framework;
+
+
+
+namespace Knot3_Unit_Tests
+{
+    [TestFixture]
+    public class UnitTest1
+    {
+        [Test]
+        public void TestMethod1()
+        {
+            Assert.IsNull(null);
+        }
+
+        [Test]
+        public void TestMethod2()
+        {
+            Assert.IsNotNull(null);
+        }
+    }
+}
+
