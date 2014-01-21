563f2d9a73f227d4f20a16b42b16d1701016dff4
PSE Knot³ <uaeti@student.kit.edu>
2014-01-21 20:08:02 +0100

    Code Format

--- Knot3/Knot3-Implementierung/Knot3/Widgets/SliderItem.cs
+++ Knot3/Knot3-Implementierung/Knot3/Widgets/SliderItem.cs
@@ -111,10 +111,10 @@ namespace Knot3.Widgets
 			coordinateLine.Y += this.ScaledSize.Y / 2;
 
 			if (this.minXSliderRectangle < 0) {
-                this.coordinateRec = this.ScaledPosition;
-                this.coordinateRec.X += this.ScaledSize.X / 2 + ((this.Value/10)* (280/this.Step));
-                this.minXSliderRectangle = coordinateLine.X;
-                this.maxXSliderRectangle = coordinateLine.X + 280;
+				this.coordinateRec = this.ScaledPosition;
+				this.coordinateRec.X += this.ScaledSize.X / 2 + ((this.Value/10)* (280/this.Step));
+				this.minXSliderRectangle = coordinateLine.X;
+				this.maxXSliderRectangle = coordinateLine.X + 280;
 
 			}
 
