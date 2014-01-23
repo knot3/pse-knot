d5964b4731c351b03c77ea0b4c166c0955dc0b77
Gerd Augsburg <gerd.augsburg@student.kit.edu>
2014-01-23 05:18:47 +0100

    Bounding Cylinder
    Mit Vorsicht testen!!!

---
+++ Knot3/Knot3-Implementierung/Knot3/Utilities/BoundingCylinder.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Microsoft.Xna.Framework;
+
+namespace Knot3.Knot3.Utilities
+{
+    public struct BoundingCylinder : IEquatable<BoundingCylinder>
+    {
+        public Vector3 SideA;
+
+        public Vector3 SideB;
+
+        public float Radius;
+
+        public BoundingCylinder(Vector3 sideA, Vector3 sideB, float radius)
+        {
+            this.SideA = sideA;
+            this.SideB = sideB;
+            this.Radius = radius;
+        }
+    }
+}
