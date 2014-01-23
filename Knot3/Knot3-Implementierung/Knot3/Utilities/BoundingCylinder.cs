4c09323304efdbd2c9fc0e0901c960ff7037a5eb
Tobias Schulz <tobias@freehal.net>
2014-01-23 06:05:58 +0100

    Equals in BoundingCylinder

--- Knot3/Knot3-Implementierung/Knot3/Utilities/BoundingCylinder.cs
+++ Knot3/Knot3-Implementierung/Knot3/Utilities/BoundingCylinder.cs
@@ -7,18 +7,49 @@ using Microsoft.Xna.Framework;
 
 namespace Knot3.Knot3.Utilities
 {
-	public struct BoundingCylinder : IEquatable<BoundingCylinder> {
+	public struct BoundingCylinder : IEquatable<BoundingCylinder>
+	{
 		public Vector3 SideA;
-
 		public Vector3 SideB;
-
 		public float Radius;
 
-		public BoundingCylinder(Vector3 sideA, Vector3 sideB, float radius)
+		public BoundingCylinder (Vector3 sideA, Vector3 sideB, float radius)
 		{
 			this.SideA = sideA;
 			this.SideB = sideB;
 			this.Radius = radius;
+		}
+
+		public static bool operator == (BoundingCylinder a, BoundingCylinder b)
+		{
+			if (System.Object.ReferenceEquals (a, b)) {
+				return true;
+			}
+			if (((object)a == null) || ((object)b == null)) {
+				return false;
+			}
+			return a.Equals (b);
+		}
+
+		public static bool operator != (BoundingCylinder a, BoundingCylinder b)
+		{
+			return !(a == b);
+		}
+
+		public bool Equals (BoundingCylinder other)
+		{
+			return other != null && SideA == other.SideA && SideB == other.SideB && Radius == other.Radius;
+		}
+
+		public override bool Equals (object other)
+		{
+			return other != null && Equals (other as BoundingCylinder);
+		}
+
+		public override int GetHashCode ()
+		{
+			// irgendwas möglichst eindeutiges
+			return (Radius * (SideA + SideB)).GetHashCode ();
 		}
 	}
 }
