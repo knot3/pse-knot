6d9e8d51f66ba483553f5ef14138f4ff54e1b880
Tobias Schulz <tobias.schulz186@gmail.com>
2014-01-26 06:27:24 +0100

    Monogame Shader Tool aktualisiert

--- Knot3/Knot3-Implementierung/Content/Monogame Shader HLSL to GLSL.cmd
+++ Knot3/Knot3-Implementierung/Content/Monogame Shader HLSL to GLSL.cmd
@@ -5,7 +5,8 @@ setlocal enabledelayedexpansion
 for %%i in (*.fx) do (
 	set file=%%~ni
 	echo !file!
-	"C:\Program Files (x86)\MSBuild\Monogame\v3.0\2mgfx.exe" !file!.fx !file!.mgfx
+	"C:\Program Files (x86)\MSBuild\Monogame\v3.0\2mgfx.exe" !file!.fx !file!_3.0.mgfx
+	"..\..\Tools\2MGFX\2mgfx.exe" !file!.fx !file!_3.1.mgfx
 )
 pause
 
