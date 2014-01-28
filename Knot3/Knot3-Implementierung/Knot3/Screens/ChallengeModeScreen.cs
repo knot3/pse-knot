d7ceea05a3d24fbaefdea10594e87101e9093000
PSE Knot³ <uaeti@student.kit.edu>
2014-01-28 10:06:03 +0100

    Code Format

--- Knot3/Knot3-Implementierung/Knot3/Screens/ChallengeModeScreen.cs
+++ Knot3/Knot3-Implementierung/Knot3/Screens/ChallengeModeScreen.cs
@@ -174,7 +174,7 @@ namespace Knot3.Screens
 			// die Linien
 			lines = new Lines (screen: this, drawOrder: DisplayLayer.Dialog, lineWidth: 2);
 			lines.AddPoints (500, 0, 500, 1000);
-			
+
 			// Status
 			state = ChallengeModeState.Start;
 		}
@@ -187,10 +187,11 @@ namespace Knot3.Screens
 		{
 			Undo.Push (_playerKnot);
 			Redo.Clear ();
-			
+
 			// Status
-			if (state == ChallengeModeState.Start)
-			state = ChallengeModeState.Running;
+			if (state == ChallengeModeState.Start) {
+				state = ChallengeModeState.Running;
+			}
 		}
 
 		private void OnUndo ()
