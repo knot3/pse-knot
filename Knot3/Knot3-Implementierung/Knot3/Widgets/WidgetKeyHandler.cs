a788b194f695e0fdd308b1da7073be1647d60d84
PSE Knot³ <uaeti@student.kit.edu>
2014-01-30 16:32:16 +0100

    Code Format

--- Knot3/Knot3-Implementierung/Knot3/Widgets/WidgetKeyHandler.cs
+++ Knot3/Knot3-Implementierung/Knot3/Widgets/WidgetKeyHandler.cs
@@ -55,7 +55,7 @@ namespace Knot3.Widgets
 				foreach (Keys key in receiver.ValidKeys) {
 					//Console.WriteLine("receiver="+receiver+", validkeys="+key+", receiver.IsKeyEventEnabled="+((dynamic)receiver).IsVisible);
 
-				if (key.IsDown ()) {
+					if (key.IsDown ()) {
 						keysInvolved.Add (key);
 						keyEvent = KeyEvent.KeyDown;
 					}
