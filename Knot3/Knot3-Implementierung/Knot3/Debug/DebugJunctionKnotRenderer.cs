33f416a29ba3a2c2c3081f7a03e14e43c5cc6e04
Tobias Schulz <tobias@freehal.net>
2014-01-31 10:32:20 +0100

    ...

---
+++ Knot3/Knot3-Implementierung/Knot3/Debug/DebugJunctionKnotRenderer.cs
@@ -0,0 +1,229 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Audio;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.GamerServices;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Media;
+using Microsoft.Xna.Framework.Net;
+using Microsoft.Xna.Framework.Storage;
+
+using Knot3.Core;
+using Knot3.Screens;
+using Knot3.RenderEffects;
+using Knot3.KnotData;
+using Knot3.Widgets;
+using Knot3.Utilities;
+using Knot3.GameObjects;
+
+namespace Knot3.Debug
+{
+	public class DebugJunctionKnotRenderer : IGameObject, IEnumerable<IGameObject>
+	{
+		#region Properties
+
+		private IGameScreen screen;
+
+		/// <summary>
+		/// Enthält Informationen über die Position des Knotens.
+		/// </summary>
+		public GameObjectInfo Info { get; private set; }
+
+		/// <summary>
+		/// Die Spielwelt, in der die 3D-Modelle erstellt werden sollen.
+		/// </summary>
+		public World World { get; set; }
+
+		/// <summary>
+		/// Die Liste der 3D-Modelle der Kantenübergänge.
+		/// </summary>
+		private List<NodeModel> nodes;
+
+		/// <summary>
+		/// Die Liste der 3D-Modelle der Kanten.
+		/// </summary>
+		private List<PipeModel> pipes;
+
+		/// <summary>
+		/// Der Zwischenspeicher für die 3D-Modelle der Kanten. Hier wird das Fabrik-Entwurfsmuster verwendet.
+		/// </summary>
+		private ModelFactory pipeFactory;
+
+		/// <summary>
+		/// Der Zwischenspeicher für die 3D-Modelle der Kantenübergänge. Hier wird das Fabrik-Entwurfsmuster verwendet.
+		/// </summary>
+		private ModelFactory nodeFactory;
+
+		/// <summary>
+		/// Die Zuordnung zwischen Kanten und den dreidimensionalen Rasterpunkten, an denen sich die die Kantenübergänge befinden.
+		/// </summary>
+		private DebugJunctionNodeMap nodeMap;
+
+		#endregion
+
+		#region Constructors
+
+		/// <summary>
+		/// Erstellt ein neues KnotRenderer-Objekt für den angegebenen Spielzustand mit den angegebenen
+		/// Spielobjekt-Informationen, die unter Anderem die Position des Knotenursprungs enthalten.
+		/// </summary>
+		public DebugJunctionKnotRenderer (IGameScreen screen, Vector3 position)
+		{
+			this.screen = screen;
+			Info = new GameObjectInfo (position: position);
+			pipes = new List<PipeModel> ();
+			nodes = new List<NodeModel> ();
+			pipeFactory = new ModelFactory ((s, i) => new PipeModel (s, i as PipeModelInfo));
+			nodeFactory = new ModelFactory ((s, i) => new NodeModel (s, i as NodeModelInfo));
+			nodeMap = new DebugJunctionNodeMap ();
+		}
+
+		#endregion
+
+		#region Methods
+
+		/// <summary>
+		/// Gibt den Ursprung des Knotens zurück.
+		/// </summary>
+		public Vector3 Center ()
+		{
+			return Info.Position;
+		}
+
+		/// <summary>
+		/// Ruft die Intersects(Ray)-Methode der Kanten, Übergänge und Pfeile auf und liefert das beste Ergebnis zurück.
+		/// </summary>
+		public GameObjectDistance Intersects (Ray ray)
+		{
+			GameObjectDistance nearest = null;
+			if (!screen.Input.GrabMouseMovement) {
+				foreach (PipeModel pipe in pipes) {
+					GameObjectDistance intersection = pipe.Intersects (ray);
+					if (intersection != null) {
+						if (intersection.Distance > 0 && (nearest == null || intersection.Distance < nearest.Distance)) {
+							nearest = intersection;
+						}
+					}
+				}
+			}
+			return nearest;
+		}
+
+		public void Render (Tuple<Direction, Direction, Direction> direction, Angles3 rotations)
+		{
+			if (direction.Item1.Axis != direction.Item2.Axis && direction.Item1.Axis != direction.Item3.Axis &&
+				direction.Item2.Axis != direction.Item3.Axis) {
+
+				NodeModelInfo.curvedJunctionBumpRotationMap2 [direction] = Tuple.Create (rotations.X, rotations.Y, rotations.Z);
+
+				nodeMap.Render (direction, rotations);
+				nodeMap.Offset = Info.Position;
+
+				CreatePipes ();
+				CreateNodes ();
+
+				World.Redraw = true;
+			}
+			else {
+				pipes.Clear ();
+				nodes.Clear ();
+			}
+		}
+
+		private void CreatePipes ()
+		{
+			pipes.Clear ();
+			foreach (Edge edge in nodeMap.Edges) {
+				PipeModelInfo info = new PipeModelInfo (nodeMap: nodeMap, knot: null, edge: edge);
+				PipeModel pipe = pipeFactory [screen, info] as PipeModel;
+				pipe.Info.IsVisible = true;
+				pipe.World = World;
+				pipes.Add (pipe);
+			}
+		}
+
+		private void CreateNodes ()
+		{
+			nodes.Clear ();
+
+			foreach (Node node in nodeMap.Nodes) {
+				List<IJunction> junctions = nodeMap.JunctionsAtNode (node);
+				// zeige zwischen zwei Kanten in der selben Richtung keinen Übergang an,
+				// wenn sie alleine an dem Kantenpunkt sind
+				if (junctions.Count == 1 && junctions [0].EdgeFrom.Direction == junctions [0].EdgeTo.Direction) {
+					continue;
+				}
+
+				foreach (NodeModelInfo junction in junctions.OfType<NodeModelInfo>()) {
+					NodeModel model = nodeFactory [screen, junction] as NodeModel;
+					model.World = World;
+					nodes.Add (model);
+				}
+			}
+		}
+
+		/// <summary>
+		/// Ruft die Update()-Methoden der Kanten, Übergänge und Pfeile auf.
+		/// </summary>
+		public void Update (GameTime time)
+		{
+			foreach (PipeModel pipe in pipes) {
+				pipe.Update (time);
+			}
+			foreach (NodeModel node in nodes) {
+				node.Update (time);
+			}
+		}
+
+		/// <summary>
+		/// Ruft die Draw()-Methoden der Kanten, Übergänge und Pfeile auf.
+		/// </summary>
+		public void Draw (GameTime time)
+		{
+			if (Info.IsVisible) {
+				Profiler.Values ["# InFrustum"] = 0;
+				Profiler.Values ["RenderEffect"] = 0;
+				Profiler.ProfileDelegate ["Pipes"] = () => {
+					foreach (PipeModel pipe in pipes) {
+						pipe.Draw (time);
+					}
+				};
+				Profiler.ProfileDelegate ["Nodes"] = () => {
+					foreach (NodeModel node in nodes) {
+						node.Draw (time);
+					}
+				};
+				Profiler.Values ["# Pipes"] = pipes.Count ();
+				Profiler.Values ["# Nodes"] = nodes.Count ();
+			}
+		}
+
+		/// <summary>
+		/// Gibt einen Enumerator der aktuell vorhandenen 3D-Modelle zurück.
+		/// [returntype=IEnumerator<IGameObject>]
+		/// </summary>
+		public IEnumerator<IGameObject> GetEnumerator ()
+		{
+			foreach (PipeModel pipe in pipes) {
+				yield return pipe;
+			}
+			foreach (NodeModel node in nodes) {
+				yield return node;
+			}
+		}
+
+		// Explicit interface implementation for nongeneric interface
+		IEnumerator IEnumerable.GetEnumerator ()
+		{
+			return GetEnumerator (); // Just return the generic version
+		}
+
+		#endregion
+	}
+}
+
---
+++ Knot3/Knot3-Implementierung/Knot3/Debug/DebugJunctionNodeMap.cs
@@ -0,0 +1,150 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Audio;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.GamerServices;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Media;
+using Microsoft.Xna.Framework.Net;
+using Microsoft.Xna.Framework.Storage;
+
+using Knot3.Core;
+using Knot3.GameObjects;
+using Knot3.Screens;
+using Knot3.RenderEffects;
+using Knot3.Widgets;
+using Knot3.Utilities;
+using Knot3.KnotData;
+
+namespace Knot3.Debug
+{
+	public class DebugJunctionNodeMap : INodeMap
+	{
+		#region Properties
+
+		private Hashtable fromMap = new Hashtable ();
+		private Hashtable toMap = new Hashtable ();
+		private Dictionary<Node, List<IJunction>> junctionMap = new Dictionary<Node, List<IJunction>> ();
+
+		/// <summary>
+		/// Die Skalierung, die bei einer Konvertierung in einen Vector3 des XNA-Frameworks durch die ToVector()-Methode der Node-Objekte verwendet wird.
+		/// </summary>
+		public int Scale { get; set; }
+
+		public IEnumerable<Edge> Edges
+		{
+			get { return _edges; }
+			set {}
+		}
+
+		private Edge[] _edges;
+
+		public Vector3 Offset { get; set; }
+
+		public Action IndexRebuilt { get; set; }
+
+		#endregion
+
+		#region Constructors
+
+		public DebugJunctionNodeMap ()
+		{
+			IndexRebuilt = () => {};
+		}
+
+		#endregion
+
+		#region Methods
+
+		public void Render (Tuple<Direction, Direction, Direction> direction, Angles3 rotations)
+		{
+			_edges = new Edge[]{
+				new Edge(direction.Item1), new Edge(direction.Item1),
+				new Edge(direction.Item2), new Edge(direction.Item2),
+				new Edge(direction.Item3), new Edge(direction.Item3),
+			};
+			BuildIndex ();
+		}
+
+		/// <summary>
+		/// Gibt die Rasterposition des Übergangs am Anfang der Kante zurück.
+		/// </summary>
+		public Node NodeBeforeEdge (Edge edge)
+		{
+			return (Node)fromMap [edge];
+		}
+
+		/// <summary>
+		/// Gibt die Rasterposition des Übergangs am Ende der Kante zurück.
+		/// </summary>
+		public Node NodeAfterEdge (Edge edge)
+		{
+			return (Node)toMap [edge];
+		}
+
+		public List<IJunction> JunctionsAtNode (Node node)
+		{
+			return junctionMap [node];
+		}
+
+		public List<IJunction> JunctionsBeforeEdge (Edge edge)
+		{
+			Node node = NodeBeforeEdge (edge);
+			return junctionMap.ContainsKey(node) ? junctionMap [node] : new List<IJunction>();
+		}
+
+		public List<IJunction> JunctionsAfterEdge (Edge edge)
+		{
+			Node node = NodeAfterEdge (edge);
+			return junctionMap.ContainsKey(node) ? junctionMap [node] : new List<IJunction>();
+		}
+
+		public IEnumerable<Node> Nodes
+		{
+			get {
+				return junctionMap.Keys;
+			}
+		}
+
+		/// <summary>
+		/// Aktualisiert die Zuordnung, wenn sich die Kanten geändert haben.
+		/// </summary>
+		public void OnEdgesChanged ()
+		{
+			BuildIndex ();
+		}
+
+		private void BuildIndex ()
+		{
+			fromMap.Clear ();
+			toMap.Clear ();
+			IndexRebuilt = () => {};
+			junctionMap.Clear ();
+
+			Node zero = new Node (0, 0, 0);
+			for (int i = 0; i <= 2; ++i) {
+				Edge edge1 = _edges [i * 2 + 0];
+				Edge edge2 = _edges [i * 2 + 1];
+
+				fromMap [edge1] = zero - edge1.Direction;
+				toMap [edge1] = zero;
+				fromMap [edge2] = zero;
+				toMap [edge2] = zero + edge2.Direction;
+
+				Node node = NodeAfterEdge (edge1);
+				IJunction junction = new NodeModelInfo (nodeMap: this, from: edge1, to: edge2, node: node, index: i * 2);
+				junctionMap.Add (node, junction);
+			}
+
+			IndexRebuilt ();
+		}
+
+		#endregion
+	}
+}
+
