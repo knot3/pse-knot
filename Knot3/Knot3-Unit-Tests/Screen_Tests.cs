87107350f96def34457641176e118f99313c0bdb
Pascal Knodel <pascal.knodel@student.kit.edu>
2014-01-31 14:30:44 +0100

    Erstelle/Bearbeite Testklassen.

---
+++ Knot3/Knot3-Unit-Tests/Screen_Tests.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+
+
+
+namespace Knot3.Screen_Tests
+{
+
+    /// <summary>
+    /// 
+    /// </summary>
+    
+
+
+    [TestClass]
+    public class Screen_Tests
+    {
+
+
+
+        [TestMethod]
+        public void Test_Undo()
+        {
+        }
+
+
+        [TestMethod]
+        public void Test_Redo()
+        {
+        }
+
+
+
+    }
+}
