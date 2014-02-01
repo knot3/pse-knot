20269fea212de56ddc8c5f035da75b7b4ce59169
Tobias Schulz <tobias@freehal.net>
2014-02-01 21:33:06 +0100

    ...

--- Knot3/Knot3-Implementierung/Knot3/Screens/CreativeLoadScreen.cs
+++ Knot3/Knot3-Implementierung/Knot3/Screens/CreativeLoadScreen.cs
@@ -142,9 +142,9 @@ namespace Knot3.Screens
 		{
 			// Finde den Namen des Knotens
 			string name = meta.Name.Length > 0 ? meta.Name : filename;
+
 			// Erstelle eine Lamdafunktion, die beim Auswählen des Menüeintrags ausgeführt wird
 			Action<GameTime> LoadFile = (time) => {
-
 				//NextScreen = n ew CreativeModeScreen (game: Game, knot: loader.FileFormat.Load (filename));
 			};
 
