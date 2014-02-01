46dd9049f4f7b0232105e8f57ee30f0d92aff4c9
Maximilian-Reuter <maximilian.reuter@t-online.de>
2014-02-02 00:53:58 +0100

    added draw capabilities to button

--- Knot3/Knot3-Implementierung/Knot3/Widgets/Button.cs
+++ Knot3/Knot3-Implementierung/Knot3/Widgets/Button.cs
@@ -2,7 +2,6 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
-
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Audio;
 using Microsoft.Xna.Framework.Content;
@@ -12,20 +11,21 @@ using Microsoft.Xna.Framework.Input;
 using Microsoft.Xna.Framework.Media;
 using Microsoft.Xna.Framework.Net;
 using Microsoft.Xna.Framework.Storage;
-
 using Knot3.Core;
 using Knot3.GameObjects;
 using Knot3.Screens;
 using Knot3.RenderEffects;
 using Knot3.KnotData;
+using Knot3.Utilities;
 
 namespace Knot3.Widgets
 {
 	/// <summary>
 	/// Eine Schaltfläche, der eine Zeichenkette anzeigt und auf einen Linksklick reagiert.
 	/// </summary>
-	public class Button : Widget
+	public sealed class Button : Widget,IKeyEventListener, IMouseClickEventListener
 	{
+
 		#region Properties
 
 		/// <summary>
@@ -33,23 +33,15 @@ namespace Knot3.Widgets
 		/// </summary>
 		public Action<GameTime> OnClick { get; set; }
 
-		/// <summary>
-		/// Wie viel Prozent der Name des Eintrags (auf der linken Seite) von der Breite des Eintrags einnehmen darf.
-		/// </summary>
-		public override float NameWidth
-		{
-			get { return 1.00f; }
-			set { throw new ArgumentException ("You can't change the NameWidth of a MenuButton!"); }
-		}
+		private string name;
+		private SpriteBatch spriteBatch;
 
-		/// <summary>
-		/// Wie viel Prozent der Wert des Eintrags (auf der rechten Seite) von der Breite des Eintrags einnehmen darf.
-		/// </summary>
-		public override float ValueWidth
-		{
-			get { return 0.00f; }
-			set { throw new ArgumentException ("You can't change the ValueWidth of a MenuButton!"); }
-		}
+		public  Rectangle MouseClickBounds { get { return Bounds; } }
+
+		public Rectangle MouseScrollBounds { get { return Bounds; } }
+
+		public Action<bool, GameTime> Hovered = (isHovered, time) => {
+		};
 
 		#endregion
 
@@ -61,9 +53,11 @@ namespace Knot3.Widgets
 		/// und der Aktion, welche bei einem Klick ausgeführt wird Pflicht.
 		/// </summary>
 		public Button (IGameScreen screen, DisplayLayer drawOrder, string name, Action<GameTime> onClick)
-		: base(screen, drawOrder, name)
+		: base (screen, drawOrder)
 		{
+			this.name = name;
 			OnClick = onClick;
+			spriteBatch = new SpriteBatch (screen.Device);
 		}
 
 		#endregion
@@ -73,16 +67,26 @@ namespace Knot3.Widgets
 		/// <summary>
 		/// Reaktionen auf einen Linksklick.
 		/// </summary>
-		public override void OnLeftClick (Vector2 position, ClickState state, GameTime time)
+		public  void OnLeftClick (Vector2 position, ClickState state, GameTime time)
 		{
-			base.OnLeftClick (position, state, time);
+			OnLeftClick (position, state, time);
 			OnClick (time);
+		}
+
+		public  void OnRightClick (Vector2 position, ClickState state, GameTime time)
+		{
+		}
+
+		public  void SetHovered (bool isHovered, GameTime time)
+		{
+			State = isHovered ? State.Hovered : State.None;
+			Hovered (isHovered, time);
 		}
 
 		/// <summary>
 		/// Reaktionen auf Tasteneingaben.
 		/// </summary>
-		public override void OnKeyEvent (List<Keys> key, KeyEvent keyEvent, GameTime time)
+		public  void OnKeyEvent (List<Keys> key, KeyEvent keyEvent, GameTime time)
 		{
 			// Console.WriteLine("OnKeyEvent: " + key[0]);
 			if (keyEvent == KeyEvent.KeyDown) {
@@ -97,6 +101,28 @@ namespace Knot3.Widgets
 			}
 		}
 
+		public override void Draw (GameTime time)
+		{
+			base.Draw (time);
+
+			if (IsVisible) {
+				spriteBatch.Begin ();
+
+				// zeichne den Hintergrund
+				spriteBatch.DrawColoredRectangle (BackgroundColor (), Bounds);
+
+				// lade die Schrift
+				SpriteFont font = HfGDesign.MenuFont (Screen);
+
+				// zeichne die Schrift
+				Color foreground = ForegroundColor () * (IsEnabled ? 1f : 0.5f);
+				spriteBatch.DrawStringInRectangle (font, name, foreground, Bounds, AlignX, AlignY);
+
+				spriteBatch.End ();
+			}
+		}
+
 		#endregion
+
 	}
 }
