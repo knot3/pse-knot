59d9253e3f1bd46fc9115d76f5b696bf7dc500df
Maximilian-Reuter <maximilian.reuter@t-online.de>
2014-02-02 00:57:16 +0100

    Code Format

--- Knot3/Knot3-Implementierung/Knot3/Widgets/Button.cs
+++ Knot3/Knot3-Implementierung/Knot3/Widgets/Button.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
+
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Audio;
 using Microsoft.Xna.Framework.Content;
@@ -11,6 +12,7 @@ using Microsoft.Xna.Framework.Input;
 using Microsoft.Xna.Framework.Media;
 using Microsoft.Xna.Framework.Net;
 using Microsoft.Xna.Framework.Storage;
+
 using Knot3.Core;
 using Knot3.GameObjects;
 using Knot3.Screens;
@@ -25,7 +27,6 @@ namespace Knot3.Widgets
 	/// </summary>
 	public sealed class Button : Widget,IKeyEventListener, IMouseClickEventListener
 	{
-
 		#region Properties
 
 		/// <summary>
@@ -40,7 +41,8 @@ namespace Knot3.Widgets
 
 		public Rectangle MouseScrollBounds { get { return Bounds; } }
 
-		public Action<bool, GameTime> Hovered = (isHovered, time) => {
+		public Action<bool, GameTime> Hovered = (isHovered, time) =>
+		{
 		};
 
 		#endregion
@@ -122,6 +124,5 @@ namespace Knot3.Widgets
 		}
 
 		#endregion
-
 	}
 }
