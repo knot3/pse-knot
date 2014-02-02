2a04cf575ab30ef1869d3a9bf3921d07b7da130b
Daniel Warzel <daniel.warzel@gmx.de>
2014-02-02 21:53:29 +0100

    Profile-Name fix

--- Knot3/Knot3-Implementierung/Knot3/Screens/ProfileSettingsScreen.cs
+++ Knot3/Knot3-Implementierung/Knot3/Screens/ProfileSettingsScreen.cs
@@ -54,11 +54,12 @@ namespace Knot3.Screens
 			settingsMenu.ItemAlignX = HorizontalAlignment.Left;
 			settingsMenu.ItemAlignY = VerticalAlignment.Center;
 
+            
 			InputItem playerNameInput = new InputItem(
 			    screen: this,
 			    drawOrder: DisplayLayer.ScreenUI + DisplayLayer.MenuItem,
 			    text: "Player Name:",
-			    inputText: "DEFAULT" // TODO
+                inputText: Options.Default["profile", "name", "Player"]
 			);
 			playerNameInput.OnValueSubmitted += () => {
 				Options.Default["profile", "name", ""] = playerNameInput.InputText;
