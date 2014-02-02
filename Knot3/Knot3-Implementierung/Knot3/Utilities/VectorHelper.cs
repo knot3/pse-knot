6aa0b0850277990aef237733d65484eab606b411
Tobias Schulz <tobias@freehal.net>
2014-02-02 02:25:57 +0100

    ...

--- Knot3/Knot3-Implementierung/Knot3/Utilities/VectorHelper.cs
+++ Knot3/Knot3-Implementierung/Knot3/Utilities/VectorHelper.cs
@@ -64,8 +64,7 @@ namespace Knot3.Utilities
 
 		public static float AngleBetween (this Vector2 a, Vector2 b)
 		{
-			return ((b.X - a.X) > 0 ? 1 : -1)
-			       * (float)Math.Acos ((double)Vector2.Dot (Vector2.Normalize (a), Vector2.Normalize (b)));
+			return ((b.X - a.X) > 0 ? 1 : -1) * (float)Math.Acos ((double)Vector2.Dot (Vector2.Normalize (a), Vector2.Normalize (b)));
 		}
 
 		public static float AngleBetween (this Vector3 a, Vector3 b)
@@ -305,10 +304,7 @@ namespace Knot3.Utilities
 		public static Rectangle Scale (this Rectangle rect, Viewport viewport)
 		{
 			Point max = viewport.ToVector2 ().ToPoint ();
-			return new Rectangle (
-			           rect.X * max.X / 1000, rect.Y * max.Y / 1000,
-			           rect.Width * max.X / 1000, rect.Height * max.Y / 1000
-			       );
+			return new Rectangle (rect.X * max.X / 1000, rect.Y * max.Y / 1000, rect.Width * max.X / 1000, rect.Height * max.Y / 1000);
 		}
 
 		public static Rectangle Grow (this Rectangle rect, int x, int y)
