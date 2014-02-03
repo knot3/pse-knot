6ce1699424182e62bb2d111d489195320eafb2fd
Pascal Knodel <pascal.knodel@student.kit.edu>
2014-02-03 23:31:13 +0100

    Code Format

--- Knot3/Knot3-Coverage-Tests-Windows/Beispiel.cs
+++ Knot3/Knot3-Coverage-Tests-Windows/Beispiel.cs
@@ -1,16 +1,12 @@
 ﻿using NUnit.Framework;
 
-
 // http://www.codeproject.com/Articles/677691/Getting-code-coverage-from-your-NET-testing-using
-
-
 
 [TestFixture]
 public class TargetTest
 {
-    [Test]
-    public void DoSomethingTest()
-    {
-       
-    }
-} 
+	[Test]
+	public void DoSomethingTest()
+	{
+	}
+}
--- Knot3/Knot3-Unit-Tests/Tests/Beispiele/NUnit_Beispiel_Tests.cs
+++ Knot3/Knot3-Unit-Tests/Tests/Beispiele/NUnit_Beispiel_Tests.cs
@@ -4,39 +4,30 @@ using NUnit.Framework;
 
 //namespace Knot3.UnitTests
 //{
-	[TestFixture]
-	public class NUnit_Beispiel_Tests
+[TestFixture]
+public class NUnit_Beispiel_Tests
+{
+
+	[Test]
+	public void Test_Methode1 ()
 	{
-		
-
-		[Test]
-		public void Test_Methode1 ()
-		{
-			Assert.IsNull (null);
-		}
-
-		[Test]
-		public void Test_Methode2 ()
-		{
-			Assert.IsNotNull (null);
-		}
-
-
-        public static void DoSomething()
-        {
-            try
-            {
-                Console.WriteLine("I Random!");
-            }
-            catch (Exception ex)
-            {
-                Console.WriteLine(ex.Message);
-            }
-
-
-        }
-
-
-   
+		Assert.IsNull (null);
 	}
+
+	[Test]
+	public void Test_Methode2 ()
+	{
+		Assert.IsNotNull (null);
+	}
+
+	public static void DoSomething()
+	{
+		try {
+			Console.WriteLine("I Random!");
+		}
+		catch (Exception ex) {
+			Console.WriteLine(ex.Message);
+		}
+	}
+}
 //}
