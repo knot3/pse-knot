d357eaafc448c1fb02c04943594b2a3c83a99e87
Tobias Schulz <tobias@freehal.net>
2014-02-04 10:56:47 +0100

    ...

--- Knot3/Knot3-Implementierung/Knot3/Development/Debug.cs
+++ Knot3/Knot3-Implementierung/Knot3/Development/Debug.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Diagnostics;
 using System.Linq;
+using Knot3.Utilities;
 
 namespace Knot3.Development
 {
@@ -10,29 +11,34 @@ namespace Knot3.Development
 		private static int lastDebugTimes = 0;
 
 		[Conditional("DEBUG")]
-		public static void Debug (params object[] strs)
+		public static void Debug (params object[] message)
 		{
-			string str = string.Join ("", strs);
-			if (str == lastDebugStr) {
-				++lastDebugTimes;
-				if (lastDebugTimes > 100) {
-					Console.WriteLine ("[" + lastDebugTimes.ToString () + "x] " + lastDebugStr);
+			string str = string.Join ("", message);
+			if (MonoHelper.IsRunningOnLinux ()) {
+				Console.WriteLine (str);
+			}
+			else {
+				if (str == lastDebugStr) {
+					++lastDebugTimes;
+					if (lastDebugTimes > 100) {
+						Console.WriteLine ("[" + lastDebugTimes.ToString () + "x] " + lastDebugStr);
+						lastDebugTimes = 0;
+					}
+				}
+				else {
+					if (lastDebugTimes > 0) {
+						Console.WriteLine (lastDebugTimes.ToString () + "x " + lastDebugStr);
+					}
+					Console.WriteLine (str);
+					lastDebugStr = str;
 					lastDebugTimes = 0;
 				}
 			}
-			else {
-				if (lastDebugTimes > 0) {
-					Console.WriteLine (lastDebugTimes.ToString () + "x " + lastDebugStr);
-				}
-				Console.WriteLine (str);
-				lastDebugStr = str;
-				lastDebugTimes = 0;
-			}
 		}
 
-		public static void Message (params object[] strs)
+		public static void Message (params object[] message)
 		{
-			Console.WriteLine (string.Join ("", strs));
+			Console.WriteLine (string.Join ("", message));
 		}
 	}
 }
--- Knot3/Knot3-Implementierung/Knot3/Program.cs
+++ Knot3/Knot3-Implementierung/Knot3/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 
 using Knot3.Core;
+using Knot3.Development;
 
 namespace Knot3
 {
@@ -18,8 +19,20 @@ namespace Knot3
 		[STAThread]
 		static void Main ()
 		{
+			Log.Message ("Knot" + Char.ConvertFromUtf32 ('\u00B3').ToString () + " " + Version);
+			Log.Message ("Copyright (C) 2013-2014 Tobias Schulz, Maximilian Reuter, Pascal Knodel, Gerd Augsburg, Christina Erler, Daniel Warzel");
+
 			game = new Knot3Game ();
 			game.Run ();
 		}
+
+		/// <summary>
+		/// Gibt die Versionsnummer zurück.
+		/// </summary>
+		/// <returns></returns>
+		public static string Version
+		{ get {
+				return System.Reflection.Assembly.GetExecutingAssembly ().GetName ().Version.ToString ();
+			} }
 	}
 }
