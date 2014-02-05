d597861bbc610a1c7aab30dd4231aa0333efa832
Tobias Schulz <tobias@freehal.net>
2014-02-05 12:26:47 +0100

    vs->nunit

--- Knot3/Knot3-Unit-Tests/Tests/Screens/Test_ControlSettingsScreen.cs
+++ Knot3/Knot3-Unit-Tests/Tests/Screens/Test_ControlSettingsScreen.cs
@@ -2,6 +2,8 @@
 using System.Text;
 using System.Collections.Generic;
 
+using NUnit.Framework;
+
 namespace Knot3.UnitTests.Tests.Screens
 {
 	/// <summary>
@@ -10,53 +12,15 @@ namespace Knot3.UnitTests.Tests.Screens
 	[TestFixture]
 	public class Test_ControlSettingsScreen
 	{
-		public Test_ControlSettingsScreen()
+		public Test_ControlSettingsScreen ()
 		{
 			//
 			// TODO: Konstruktorlogik hier hinzufügen
 			//
 		}
 
-		private TestContext testContextInstance;
-
-		/// <summary>
-		///Ruft den Textkontext mit Informationen über
-		///den aktuellen Testlauf sowie Funktionalität für diesen auf oder legt diese fest.
-		///</summary>
-		public TestContext TestContext
-		{
-			get {
-				return testContextInstance;
-			}
-			set {
-				testContextInstance = value;
-			}
-		}
-
-		#region Zusätzliche Testattribute
-		//
-		// Sie können beim Schreiben der Tests folgende zusätzliche Attribute verwenden:
-		//
-		// Verwenden Sie ClassInitialize, um vor Ausführung des ersten Tests in der Klasse Code auszuführen.
-		// [ClassInitialize()]
-		// public static void MyClassInitialize(TestContext testContext) { }
-		//
-		// Verwenden Sie ClassCleanup, um nach Ausführung aller Tests in einer Klasse Code auszuführen.
-		// [ClassCleanup()]
-		// public static void MyClassCleanup() { }
-		//
-		// Mit TestInitialize können Sie vor jedem einzelnen Test Code ausführen.
-		// [TestInitialize()]
-		// public void MyTestInitialize() { }
-		//
-		// Mit TestCleanup können Sie nach jedem einzelnen Test Code ausführen.
-		// [TestCleanup()]
-		// public void MyTestCleanup() { }
-		//
-		#endregion
-
 		[Test]
-		public void Test1()
+		public void TestMethod1 ()
 		{
 			//
 			// TODO: Testlogik hier hinzufügen
