c6c5d71b17edd89084dedf5b24637ed68652bcff
Pascal Knodel <pascal.knodel@student.kit.edu>
2014-02-05 12:49:06 +0100

    Code Format

--- Knot3/Knot3-Unit-Tests/Tests/KnotData/Test_Challenge.cs
+++ Knot3/Knot3-Unit-Tests/Tests/KnotData/Test_Challenge.cs
@@ -1,69 +1,68 @@
 ﻿using System;
 using System.Text;
 using System.Collections.Generic;
-using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+using NUnit.Framework;
 
 namespace Knot3.UnitTests.Tests.KnotData
 {
-    /// <summary>
-    /// Zusammenfassungsbeschreibung für Test_Challenge
-    /// </summary>
-    [TestClass]
-    public class Test_Challenge
-    {
-        public Test_Challenge()
-        {
-            //
-            // TODO: Konstruktorlogik hier hinzufügen
-            //
-        }
+	/// <summary>
+	/// Zusammenfassungsbeschreibung für Test_Challenge
+	/// </summary>
+	[TestFixture]
+	public class Test_Challenge
+	{
+		public Test_Challenge()
+		{
+			//
+			// TODO: Konstruktorlogik hier hinzufügen
+			//
+		}
 
-        private TestContext testContextInstance;
+		private TestContext testContextInstance;
 
-        /// <summary>
-        ///Ruft den Textkontext mit Informationen über
-        ///den aktuellen Testlauf sowie Funktionalität für diesen auf oder legt diese fest.
-        ///</summary>
-        public TestContext TestContext
-        {
-            get
-            {
-                return testContextInstance;
-            }
-            set
-            {
-                testContextInstance = value;
-            }
-        }
+		/// <summary>
+		///Ruft den Textkontext mit Informationen über
+		///den aktuellen Testlauf sowie Funktionalität für diesen auf oder legt diese fest.
+		///</summary>
+		public TestContext TestContext
+		{
+			get {
+				return testContextInstance;
+			}
+			set {
+				testContextInstance = value;
+			}
+		}
 
-        #region Zusätzliche Testattribute
-        //
-        // Sie können beim Schreiben der Tests folgende zusätzliche Attribute verwenden:
-        //
-        // Verwenden Sie ClassInitialize, um vor Ausführung des ersten Tests in der Klasse Code auszuführen.
-        // [ClassInitialize()]
-        // public static void MyClassInitialize(TestContext testContext) { }
-        //
-        // Verwenden Sie ClassCleanup, um nach Ausführung aller Tests in einer Klasse Code auszuführen.
-        // [ClassCleanup()]
-        // public static void MyClassCleanup() { }
-        //
-        // Mit TestInitialize können Sie vor jedem einzelnen Test Code ausführen. 
-        // [TestInitialize()]
-        // public void MyTestInitialize() { }
-        //
-        // Mit TestCleanup können Sie nach jedem einzelnen Test Code ausführen.
-        // [TestCleanup()]
-        // public void MyTestCleanup() { }
-        //
-        #endregion
+		#region Zusätzliche Testattribute
+		//
+		// Sie können beim Schreiben der Tests folgende zusätzliche Attribute verwenden:
+		//
+		// Verwenden Sie ClassInitialize, um vor Ausführung des ersten Tests in der Klasse Code auszuführen.
+		// [ClassInitialize()]
+		// public static void MyClassInitialize(TestContext testContext) { }
+		//
+		// Verwenden Sie ClassCleanup, um nach Ausführung aller Tests in einer Klasse Code auszuführen.
+		// [ClassCleanup()]
+		// public static void MyClassCleanup() { }
+		//
+		// Mit TestInitialize können Sie vor jedem einzelnen Test Code ausführen.
+		// [TestInitialize()]
+		// public void MyTestInitialize() { }
+		//
+		// Mit TestCleanup können Sie nach jedem einzelnen Test Code ausführen.
+		// [TestCleanup()]
+		// public void MyTestCleanup() { }
+		//
+		#endregion
 
-        [TestMethod]
-        public void TestMethod1()
-        {
-            //
-            // TODO: Testlogik hier hinzufügen
-            //
-        }
-    }
+		[Test]
+		public void TestMethod1()
+		{
+			//
+			// TODO: Testlogik hier hinzufügen
+			//
+		}
+	}
 }
--- Knot3/Knot3-Unit-Tests/Tests/KnotData/Test_KnotStringIO.cs
+++ Knot3/Knot3-Unit-Tests/Tests/KnotData/Test_KnotStringIO.cs
@@ -4,27 +4,22 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-
 using NUnit.Framework;
-
 
 using Knot3.KnotData;
 
-
 namespace Knot3.UnitTests.Tests.KnotData
 {
-    class Test_KnotStringIO
-    {
+	class Test_KnotStringIO
+	{
+		[TestFixture]
+		public void test_EncodeDecodeEdge()
+		{
+			Edge[] edges = {Edge.Left, Edge.Right, Edge.Up, Edge.Down};
 
-        [TestFixture]
-        public void test_EncodeDecodeEdge()
-        {
-            Edge[] edges = {Edge.Left, Edge.Right, Edge.Up, Edge.Down};
-
-            foreach (Edge edge in edges) {
-                
-                // EncodeEdge(edge);
-            }
-        }
-    }
+			foreach (Edge edge in edges) {
+				// EncodeEdge(edge);
+			}
+		}
+	}
 }
