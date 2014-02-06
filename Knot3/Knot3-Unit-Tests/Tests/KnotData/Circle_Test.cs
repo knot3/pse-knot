3f7d1715e980b35fa9d5ee27cb61d21d71d4ac8a
Gerd Augsburg <g.augsburg@gmx.net>
2014-02-06 18:32:23 +0100

    First circle test

---
+++ Knot3/Knot3-Unit-Tests/Tests/KnotData/Circle_Test.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using NUnit.Framework;
+
+using Knot3.KnotData;
+
+namespace Knot3.UnitTests.Tests.KnotData
+{
+    [TestFixture]
+    class Circle_Test
+    {
+        public Circle_Test()
+        {
+        }
+        [Test]
+        public void Constructor_Tests()
+        {
+            CircleEntry<int> start = new CircleEntry<int>(new int[] {0, 1, 2, 3, 4, 5, 6, 7, 8, 9 });
+            for (int n = 0; n < 10; n++)
+            {
+                Assert.AreEqual(start.Value, n);
+                start = start.Next;
+            }
+        }
+    }
+}
