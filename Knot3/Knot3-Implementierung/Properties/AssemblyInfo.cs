83e82fc08bd8c16854bff025ed27b84cec0bff79
Gerd Augsburg <gerd.augsburg@student.kit.edu>
2014-02-08 01:08:06 +0100

    Code Format

--- Knot3/Knot3-Implementierung/Properties/AssemblyInfo.cs
+++ Knot3/Knot3-Implementierung/Properties/AssemblyInfo.cs
@@ -35,5 +35,5 @@ using System.Runtime.InteropServices;
 // übernehmen, indem Sie "*" eingeben:
 // [assembly: AssemblyVersion("1.0.*")]
 
-[assembly: AssemblyVersion("1.1.109.0")]
-[assembly: AssemblyFileVersion("1.1.109.0")]
+[assembly: AssemblyVersion("1.1.110.0")]
+[assembly: AssemblyFileVersion("1.1.110.0")]
