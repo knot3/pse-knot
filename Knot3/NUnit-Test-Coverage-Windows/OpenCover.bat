1ceb807db4ddb23b143562b2b2baa32644de3055
Pascal Knodel <pascal.knodel@student.kit.edu>
2014-02-08 15:59:46 +0100

    Lokales Coverage-Tool "fast" funktionsfähig.

--- Knot3/NUnit-Test-Coverage-Windows/OpenCover.bat
+++ Knot3/NUnit-Test-Coverage-Windows/OpenCover.bat
@@ -2,6 +2,17 @@ echo.
 echo --- OpenCover.bat ---
 echo.
 ::
+:: Folgende Tools müssen installiert sein:
+::
+:: !!!!!!!!!!!!!!!!!!!!!!!!!
+::
+:: NUnit (2.6.3)
+:: OpenCover (4.5.1923)
+:: ReportGenerator (dort hin tun: %ProgramFiles(x86)%\ReportGenerator)
+::
+:: !!!!!!!!!!!!!!!!!!!!!!!!!
+::
+::
 :: Visual Studio: 
 ::
 :: 1. Einstellungen treffen: (Projektmappe)
@@ -17,12 +28,13 @@ echo.
 ::
 :: ...
 ::
-set PATH_TO_NUNIT="%ProgramFiles(x86)%\NUnit 2.6.3\bin"
 set PATH_TO_OPENCOVER="%USERPROFILE%\AppData\Local\Apps\OpenCover"
+set PATH_TO_NUNIT="%ProgramFiles(x86)%\NUnit 2.6.3\bin"
 cd ..
 cd ..
 cd ..
-set PATH_TO_PROJECT="%CD%\Knot3-Unit-Tests\bin\Debug"
+set PATH_TO_PROJECT="%CD%\Knot3-Unit-Tests\bin\Debug\Knot3.exe"
+set PATH_TO_REPORTGENERATOR="%ProgramFiles(x86)%\ReportGenerator\bin"
 echo.
 echo.
 echo Settings:
@@ -30,8 +42,19 @@ echo.
 echo Current path: %CD%
 echo.
 echo Paths to ...
-echo  NUnit      : %PATH_TO_NUNIT%
-echo  OpenCover  : %PATH_TO_OPENCOVER%
-echo  VS-Project : %PATH_TO_PROJECT%
+echo  NUnit           : %PATH_TO_NUNIT%
+echo  OpenCover       : %PATH_TO_OPENCOVER%
+echo  VS-Project      : %PATH_TO_PROJECT%
+echo  ReportGenerator : %PATH_TO_REPORTGENERATOR%
 echo.
-%PATH_TO_OPENCOVER%\OpenCover.Console.exe -target:%PATH_TO_NUNIT%\nunit-console.exe -targetargs:"/noshadow %CD%\Assembly.exe" -filter:"+[*]*" -output:NUnit_test_coverage.xml
+::
+:: Hinweis: Auf einem 64-Bit-System ist nunit-console-x86.exe für 32-Bit-Projekte zu verwenden! (sonst tritt ein Fehler auf)
+::
+%PATH_TO_OPENCOVER%\OpenCover.Console.exe -target:%PATH_TO_NUNIT%\nunit-console-x86.exe -targetargs:"/noshadow %PATH_TO_PROJECT%" -filter:"+[*]*" -output:NUnit_test_coverage.xml
+echo.
+echo Current path: %CD%
+echo.
+set PATH_TO_REPORT="%CD%\NUnit-Test-Coverage-Windows\bin\Debug"
+echo Path to report: %PATH_TO_REPORT%
+echo.
+%PATH_TO_REPORTGENERATOR%\ReportGenerator.exe %PATH_TO_REPORT%\NUnit_test_coverage.xml %PATH_TO_REPORT%\coverage_report
