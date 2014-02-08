27dcfecd9522e1ac755f947226278e80336ae357
Daniel Warzel <daniel.warzel@gmx.de>
2014-02-08 13:01:08 +0100

    Code Format

--- Knot3/Knot3-Unit-Tests/Tests/Widgets/Bounds_Tests.cs
+++ Knot3/Knot3-Unit-Tests/Tests/Widgets/Bounds_Tests.cs
@@ -40,34 +40,34 @@ namespace Knot3.UnitTests.Tests.Widgets
 		public void Bounds_FromDirection_Test()
 		{
 			Bounds compareBound = new Bounds(point, new ScreenPoint(fakeScreen,1f, 0.9f));
-            Assert.AreEqual(true, boundsEqual(compareBound, bound.FromTop(0.9f)), "top");
+			Assert.AreEqual(true, boundsEqual(compareBound, bound.FromTop(0.9f)), "top");
 
-            //Irgendwie will er hier bei bottom, right und left noch nicht mit der boundsEquals arbeiten habe es nun noch nicht geändert
-            compareBound = new Bounds(new ScreenPoint(fakeScreen, 0f, 0.1f), new ScreenPoint(fakeScreen, 1f, 0.9f));
-            Assert.AreEqual(compareBound.ToString(), bound.FromBottom(0.9f).ToString(), "bottom");
-            compareBound = new Bounds(new ScreenPoint(fakeScreen, 0.1f, 0f), new ScreenPoint(fakeScreen, 0.9f, 1f));
-            Assert.AreEqual(compareBound.ToString(), bound.FromRight(0.9f).ToString(), "right");
-            compareBound = new Bounds(point, new ScreenPoint(fakeScreen, 0.9f, 1f));
-            Assert.AreEqual(compareBound.ToString(), bound.FromLeft(0.9f).ToString(), "left");
-
+			//Irgendwie will er hier bei bottom, right und left noch nicht mit der boundsEquals arbeiten habe es nun noch nicht geändert
+			compareBound = new Bounds(new ScreenPoint(fakeScreen, 0f, 0.1f), new ScreenPoint(fakeScreen, 1f, 0.9f));
+			Assert.AreEqual(compareBound.ToString(), bound.FromBottom(0.9f).ToString(), "bottom");
+			compareBound = new Bounds(new ScreenPoint(fakeScreen, 0.1f, 0f), new ScreenPoint(fakeScreen, 0.9f, 1f));
+			Assert.AreEqual(compareBound.ToString(), bound.FromRight(0.9f).ToString(), "right");
+			compareBound = new Bounds(point, new ScreenPoint(fakeScreen, 0.9f, 1f));
+			Assert.AreEqual(compareBound.ToString(), bound.FromLeft(0.9f).ToString(), "left");
 		}
 
-        [Test]
-        public void Bounds_Set_Test()
-        {
-            ScreenPoint newSize = new ScreenPoint(fakeScreen, 1f, 0.9f);
-            Bounds compareBound = new Bounds(point, newSize);
-            bound.Size = newSize;
-            Assert.AreEqual(true, boundsEqual(compareBound, bound));
-        }
+		[Test]
+		public void Bounds_Set_Test()
+		{
+			ScreenPoint newSize = new ScreenPoint(fakeScreen, 1f, 0.9f);
+			Bounds compareBound = new Bounds(point, newSize);
+			bound.Size = newSize;
+			Assert.AreEqual(true, boundsEqual(compareBound, bound));
+		}
 
-        private bool boundsEqual (Bounds a, Bounds b)
-        {
-            if (a.Position.Equals(b.Position) && a.Size.Equals(b.Size)) {
-                return true;
-            } else {
-                return false;
-            }
-        }
+		private bool boundsEqual (Bounds a, Bounds b)
+		{
+			if (a.Position.Equals(b.Position) && a.Size.Equals(b.Size)) {
+				return true;
+			}
+			else {
+				return false;
+			}
+		}
 	}
 }
