b057747682568eb615be2e0648ea559de768626f
Christina <Elaine10j@aol.com>
2014-02-08 10:15:21 +0100

    Angles Test

--- Knot3/Knot3-Coverage-Tests-Windows/TargetTest.cs
+++ Knot3/Knot3-Coverage-Tests-Windows/TargetTest.cs
@@ -1,10 +1,10 @@
-﻿using System;
-using NUnit.Framework;
+﻿using NUnit.Framework;
+using System;
 
 public class TargetTest
 {
 	public void DoSomethingTest()
 	{
-		Target.DoSomething();
+		//Target.DoSomething();
 	}
 }
--- Knot3/Knot3-Unit-Tests/Tests/Core/Angles3_Tests.cs
+++ Knot3/Knot3-Unit-Tests/Tests/Core/Angles3_Tests.cs
@@ -4,6 +4,8 @@ using System.Collections.Generic;
 
 using NUnit.Framework;
 
+using Knot3.Core;
+
 namespace Knot3.UnitTests.Tests.Core
 {
 	/// <summary>
@@ -11,13 +13,36 @@ namespace Knot3.UnitTests.Tests.Core
 	/// </summary>
 	[TestFixture]
 	public class Test_Angles3
-	{
+    {
+        float X;
+        float Y;
+        float Z;
+        float redianX;
+        float redianY;
+        float redianZ;
+        Angles3 angle1;
+        Angles3 angle2;
+
+
+        [SetUp]
+        public void Init()
+        {
+            X = 120;
+            Y = 50;
+            Z = 280;
+            redianX = X * ((float)Math.PI / 180);
+            redianY = Y * ((float)Math.PI / 180);
+            redianZ = Z * ((float)Math.PI / 180);
+            angle1 = new Angles3(redianX, redianY, redianZ);
+            
+        }
+
 		[Test]
-		public void Test1()
+        public void Angles3_FromDegrees_Test()
 		{
-			//
-			// TODO: Testlogik hier hinzufügen
-			//
+            angle2 = Angles3.FromDegrees(X, Y, Z);
+            Assert.AreEqual(angle1, angle2,"miau");
+
 		}
 	}
 }
