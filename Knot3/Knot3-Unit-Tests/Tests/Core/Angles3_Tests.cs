74cde21076c4b903bf9cfc8dfb703d66196d05e2
Christina <Elaine10j@aol.com>
2014-02-09 11:24:28 +0100

    angle test

--- Knot3/Knot3-Unit-Tests/Tests/Core/Angles3_Tests.cs
+++ Knot3/Knot3-Unit-Tests/Tests/Core/Angles3_Tests.cs
@@ -21,6 +21,8 @@ namespace Knot3.UnitTests.Tests.Core
 		float redianX;
 		float redianY;
 		float redianZ;
+        float scaleFactor;
+        float divider;
 		int hash1;
 		int hash2;
 		Angles3 angle1;
@@ -47,7 +49,10 @@ namespace Knot3.UnitTests.Tests.Core
 			redian = new Vector3(redianX, redianY, redianZ);
 			angle1 = new Angles3(redian);
 			angle3 = new Angles3(rX, rY, rZ);
+
 			obj = angle1;
+            scaleFactor = 2.5f;
+            divider = 1;
 		}
 
 		[Test]
@@ -86,5 +91,37 @@ namespace Knot3.UnitTests.Tests.Core
 			hash2 = 7;
 			Assert.AreEqual(hash1, hash2);
 		}
+
+        [Test]
+        public void Angles3_Operator_Test()
+        {
+            Angles3 angle2 = new Angles3(1, 2, 3);
+            Angles3 angle4 = new Angles3(3, 2, 1);
+            Angles3 sum = angle2 + angle4;
+            Assert.AreEqual(sum, new Angles3(4,4,4));
+            Angles3 neg = -angle2;
+            Assert.AreEqual(neg, new Angles3(-1,-2,-3));
+            Angles3 diff = angle2 - angle4;
+            Assert.AreEqual(diff, new Angles3(-2,0,2));
+            Angles3 prod = angle2 * angle4;
+            Assert.AreEqual(prod, new Angles3(3,4,3));
+            Angles3 scale1 = angle2 * scaleFactor;
+            Angles3 scale2 = scaleFactor * angle2;
+            Assert.AreEqual(scale1, new Angles3(2.5f,5,7.5f));
+            Assert.AreEqual(scale2, new Angles3(2.5f, 5, 7.5f));
+            Angles3 quot1 = angle2 / angle4;
+            Assert.AreEqual(quot1, new Angles3(0.33333333333f, 1, 3));
+            Angles3 quot2 = angle2 / divider;
+            Assert.AreEqual(quot2, angle2);
+            bool same1 = (angle2 == angle2);
+            bool nsame1 = (angle2 == angle4);
+            Assert.AreEqual(true, same1);
+            Assert.AreEqual(false, nsame1);
+            bool same2 = (angle2 != angle2);
+            bool nsame2 = (angle2 != angle4);
+            Assert.AreEqual(false, same2);
+            Assert.AreEqual(true, nsame2);
+       
+        }
 	}
 }
