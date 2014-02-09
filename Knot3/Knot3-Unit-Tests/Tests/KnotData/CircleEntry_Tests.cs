852a8b742f16e4a7ee25801503db233c8aaddbda
Gerd Augsburg <gerd.augsburg@student.kit.edu>
2014-02-09 16:52:49 +0100

    CircleEntry Constructor

--- Knot3/Knot3-Unit-Tests/Tests/KnotData/CircleEntry_Tests.cs
+++ Knot3/Knot3-Unit-Tests/Tests/KnotData/CircleEntry_Tests.cs
@@ -3,6 +3,7 @@ using System.Text;
 using System.Collections.Generic;
 
 using NUnit.Framework;
+using Knot3.KnotData;
 
 namespace Knot3.UnitTests.Tests.KnotData
 {
@@ -18,6 +19,16 @@ namespace Knot3.UnitTests.Tests.KnotData
 			// TODO: Konstruktorlogik hier hinzufügen
 			//
 		}
+
+        public void Constructor_Tests()
+        {
+            CircleEntry<int> start = new CircleEntry<int>(new int[] { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 });
+            for (int n = 0; n < 10; n++)
+            {
+                Assert.AreEqual(start.Value, n);
+                start = start.Next;
+            }
+        }
 
 		[Test]
 		public void TestMethod1 ()
