88680e6993afc7adb649ccfe68b116788a7454a8
Gerd Augsburg <gerd.augsburg@student.kit.edu>
2014-02-09 16:55:08 +0100

    Code Format

--- Knot3/Knot3-Unit-Tests/Tests/KnotData/CircleEntry_Tests.cs
+++ Knot3/Knot3-Unit-Tests/Tests/KnotData/CircleEntry_Tests.cs
@@ -3,6 +3,7 @@ using System.Text;
 using System.Collections.Generic;
 
 using NUnit.Framework;
+
 using Knot3.KnotData;
 
 namespace Knot3.UnitTests.Tests.KnotData
@@ -20,15 +21,14 @@ namespace Knot3.UnitTests.Tests.KnotData
 			//
 		}
 
-        public void Constructor_Tests()
-        {
-            CircleEntry<int> start = new CircleEntry<int>(new int[] { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 });
-            for (int n = 0; n < 10; n++)
-            {
-                Assert.AreEqual(start.Value, n);
-                start = start.Next;
-            }
-        }
+		public void Constructor_Tests()
+		{
+			CircleEntry<int> start = new CircleEntry<int>(new int[] { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 });
+			for (int n = 0; n < 10; n++) {
+				Assert.AreEqual(start.Value, n);
+				start = start.Next;
+			}
+		}
 
 		[Test]
 		public void TestMethod1 ()
